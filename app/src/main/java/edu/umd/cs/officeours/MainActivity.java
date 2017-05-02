905e62308aa70a22583a8b97b2ac4f9105b247d0
Wenhao Wang <whwang_michael@hotmail.com>
2017-05-02 18:00:51 -0400

    Update MainActivity

--- app/src/main/java/edu/umd/cs/officeours/MainActivity.java
+++ app/src/main/java/edu/umd/cs/officeours/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.umd.cs.officeours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,42 +18,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=(Button) findViewById(R.id.button1);
-        b2=(Button) findViewById(R.id.button2);
-        b3=(Button) findViewById(R.id.button3);
-        b4=(Button) findViewById(R.id.button4);
-        b5=(Button) findViewById(R.id.button5);
+        b1=(Button) findViewById(R.id.Available_Knock_Button);
+        b2=(Button) findViewById(R.id.Prof_Back_In_Five_Min_Button);
+        b3=(Button) findViewById(R.id.Open_Prof_Interface_Button);
+        b4=(Button) findViewById(R.id.Away_Button);
+        b5=(Button) findViewById(R.id.Do_Not_Disturb_button);
         b1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "task1",
+                Toast.makeText(getApplicationContext(), "Knock on my door",
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         b2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "task2",
+                Toast.makeText(getApplicationContext(), "I will be back in few minutes",
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         b3.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "task3",
-                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, ProfActivity.class);
+                startActivity(intent);
             }
         });
 
         b4.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "task4",
+                Toast.makeText(getApplicationContext(), "Away",
                         Toast.LENGTH_SHORT).show();
             }
         });
 
         b5.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                Toast.makeText(getApplicationContext(), "task5",
+                Toast.makeText(getApplicationContext(), "Do_Not_Disturb",
                         Toast.LENGTH_SHORT).show();
             }
         });
