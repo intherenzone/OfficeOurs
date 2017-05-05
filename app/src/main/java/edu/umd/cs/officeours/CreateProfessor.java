c69b45a662405376fda6a4fcb4811e177d50111c
Wenhao Wang <whwang_michael@hotmail.com>
2017-05-05 19:12:05 -0400

    Update the create Professor page layout

---
+++ app/src/main/java/edu/umd/cs/officeours/CreateProfessor.java
@@ -0,0 +1,13 @@
+package edu.umd.cs.officeours;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProfessor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_professor);
+    }
+}
