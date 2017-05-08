9f150d4908b2346f20e4a32416faabafebebd6f2
Wenhao Wang <whwang_michael@hotmail.com>
2017-05-08 15:33:26 -0400

    update bio layout

--- app/src/main/java/edu/umd/cs/officeours/SelectProfFragment.java
+++ app/src/main/java/edu/umd/cs/officeours/SelectProfFragment.java
@@ -1,7 +1,6 @@
 package edu.umd.cs.officeours;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -18,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
