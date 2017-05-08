446a379444f44643530f1ab3a778ded25817f254
DanielTrevino <dtrevino@terpmail.umd.edu>
2017-05-08 17:50:48 -0400

    Fully implemented mapfragment/mapactivity

--- app/src/main/java/edu/umd/cs/officeours/MapFragment.java
+++ app/src/main/java/edu/umd/cs/officeours/MapFragment.java
@@ -1,6 +1,9 @@
 package edu.umd.cs.officeours;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.support.v4.app.Fragment;
+import android.widget.LinearLayout;
 
 public class MapFragment extends Fragment {
 
@@ -25,7 +29,8 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        final View view = inflater.inflate(R.layout.fragment_map, container, false);
+
         Button home = (Button) view.findViewById(R.id.home_button);
         Button first_floor_button = (Button) view.findViewById(R.id.first_floor_button);
         Button second_floor_button = (Button) view.findViewById(R.id.second_floor_button);
@@ -35,27 +40,70 @@ public class MapFragment extends Fragment {
 //        map.setTag(R.drawable.first_floor);
 //
 //
+        home.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                getActivity().finish();
+            }
+        });
+
         first_floor_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                LinearLayout mapLayout = (LinearLayout) view.findViewById(R.id.map_layout);
+                ImageView mapImageView = new ImageView(getActivity().getApplicationContext());
+                int height =  view.findViewById(R.id.MAP_ID).getHeight();
+                int width =  view.findViewById(R.id.MAP_ID).getWidth();
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.first_floor);
+                mapImageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap,width,height,true));
+                mapImageView.setId(R.id.MAP_ID);
+                mapLayout.removeView(view.findViewById(R.id.MAP_ID));
+                mapLayout.addView(mapImageView);
 
             }
         });
-//
-//        next_button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                if((Integer)map.getTag()==R.drawable.first_floor){
-//                    map.setImageResource(R.drawable.second_floor);
-//                    map.setTag(R.drawable.second_floor);
-//                } else if((Integer)map.getTag()==R.drawable.ground_floor){
-//                    map.setImageResource(R.drawable.first_floor);
-//                    map.setTag(R.drawable.first_floor);
-//                }
-//            }
-//        });
-//
-//
-//        return view;
+        second_floor_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout mapLayout = (LinearLayout) view.findViewById(R.id.map_layout);
+                ImageView mapImageView = new ImageView(getActivity().getApplicationContext());
+                ImageView mapView = (ImageView) view.findViewById(R.id.MAP_ID);
+                int height =  mapView.getHeight();
+                int width =  mapView.getWidth();
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.second_floor);
+                mapImageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap,width,height,true));
+                mapImageView.setId(R.id.MAP_ID);
+                mapLayout.removeView(view.findViewById(R.id.MAP_ID));
+                mapLayout.addView(mapImageView);
+
+            }
+        });
+        third_floor_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout mapLayout = (LinearLayout) view.findViewById(R.id.map_layout);
+                ImageView mapImageView = new ImageView(getActivity().getApplicationContext());
+                int height =  view.findViewById(R.id.MAP_ID).getHeight();
+                int width =  view.findViewById(R.id.MAP_ID).getWidth();
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.third_floor);
+                mapImageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap,width,height,true));
+                mapImageView.setId(R.id.MAP_ID);
+                mapLayout.removeView(view.findViewById(R.id.MAP_ID));
+                mapLayout.addView(mapImageView);
+
+            }
+        });
+        fourth_floor_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                LinearLayout mapLayout = (LinearLayout) view.findViewById(R.id.map_layout);
+                ImageView mapImageView = new ImageView(getActivity().getApplicationContext());
+                int height =  view.findViewById(R.id.MAP_ID).getHeight();
+                int width =  view.findViewById(R.id.MAP_ID).getWidth();
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.fourth_floor);
+                mapImageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap,width,height,true));
+                mapImageView.setId(R.id.MAP_ID);
+                mapLayout.removeView(view.findViewById(R.id.MAP_ID));
+                mapLayout.addView(mapImageView);
+
+            }
+        });
+
         return view;
     }
 }
